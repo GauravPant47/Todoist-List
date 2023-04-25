2cdc192e00fcfb3138a779fe8644e3031b6cb08d
Gaurav <gaurav556pant@gmail.com>
2023-04-25 11:33:20 +0530

    Updating and creating a Todoist_Data.java and creating a mysqlTosist.sql file to update a databases quary

---
+++ Todoist/src/main/java/net/Todoist/model/Todoist_Data.java
@@ -0,0 +1,78 @@
+package net.Todoist.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "todoist")
+public class Todoist_Data {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "task_number")
+	private int taskNumber;
+	
+	@Column(name = "task_tital")
+	private String taskTital;
+	
+	@Column(name = "add_Task")
+	private String addTask;
+	
+	@Column(name = "status")
+	private String status;
+	
+	
+	
+	
+	
+	
+
+	public Todoist_Data() {
+		
+	}
+
+	public Todoist_Data(String taskTital, String addTask, String status) {
+		super();
+		this.taskTital = taskTital;
+		this.addTask = addTask;
+		this.status = status;
+	}
+
+	public int getTaskNumber() {
+		return taskNumber;
+	}
+
+	public void setTaskNumber(int taskNumber) {
+		this.taskNumber = taskNumber;
+	}
+
+	public String getTaskTital() {
+		return taskTital;
+	}
+
+	public void setTaskTital(String taskTital) {
+		this.taskTital = taskTital;
+	}
+
+	public String getAddTask() {
+		return addTask;
+	}
+
+	public void setAddTask(String addTask) {
+		this.addTask = addTask;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	
+}
