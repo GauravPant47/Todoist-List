0dc2ef1d2be4827c81989308fe8fc98963ca884e
Gaurav <gaurav556pant@gmail.com>
2023-04-25 22:37:30 +0530

    Creating a create_todoistdata.html and updating controller

--- Todoist/src/main/java/net/Todoist/controller/TodoistController.java
+++ Todoist/src/main/java/net/Todoist/controller/TodoistController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import net.Todoist.model.TodoistData;
@@ -21,5 +22,14 @@ public class TodoistController {
 	public List<TodoistData> findingAList(){
 		List<TodoistData> todoist = todoistService.findAllListData();
 		return todoist;
+	}
+	
+	@GetMapping("/todoist/listItem")
+	public String createATodoistForm(Model model){
+		TodoistData todoistData = new TodoistData();
+		
+		model.addAttribute("todoistData", todoistData);
+		return "create_todoistData";
+		
 	}
 }
